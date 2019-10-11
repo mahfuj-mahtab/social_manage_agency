03d9edb7f8668c2e9a9a839866f01e8e3c5a583e
mahfuj mahtab mohot <30542294+mahfuj-mahtab@users.noreply.github.com>
2019-10-11 00:34:55 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home-Metrosoftonic.com</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/5d4ee011b6.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Arimo&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Heebo&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Heebo&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/responsive.css">
+</head>
+<body>
+    <div class="bar"><i class="fas fa-bars"></i></div>
+    <div class="header">
+        <div class="header1">
+            <div class="left_header1"><a href="">Logo</a></div>
+            <div class="right_header1">
+                <div id="header_section1">
+                    <ul>
+                        <li><span class="span1">Email Us</span> <a href="mailto:team@metrosoftonic.com"><i class="fas fa-envelope"></i>team@metrosoftonic.com</a></li>
+                       
+                    </ul>
+                    
+                </div>
+                <div id="header_section2">
+                     
+                    <ul>
+                        <li><span>Call Us</span> <a href="callto:01966213571"><i class="fas fa-phone-alt"></i>01789789571</a></li>
+                       
+                    </ul>
+                    
+                    
+                </div>
+                <div id="header_section3">
+                    
+                    <ul>
+                        <li><span>Follow us</span>
+                        
+                        <ul>
+                            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                            
+                        </ul>
+                        
+                        
+                        </li>
+                    </ul>
+                    
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div class="header2">
+           <div class="left_header2">
+            <ul>
+                <li><a href=""><i class="fas fa-home"></i>Home</a></li>
+                <li><a href="#aboutid"><i class="fas fa-address-card"></i>about</a></li>
+                <li><a href="#teamid"><i class="fas fa-users"></i>team</a></li>
+                <li><a href="#contactid">contact<i class="fas fa-arrow-down"></i></a></li>
+                <li><a  href="#serviceid">service<i class="fas fa-arrow-down"></i></a></li>
+                
+            </ul>
+            
+            </div>
+            
+            <div class="right_header2">
+                <ul>
+                    <li><a href="#newsletterid">get a free quote</a></li>
+                    <li><div class="arrow"></div></li>
+                </ul>
+            </div>
+            
+        </div>
+        
+    </div>
+    
+    <div class="banner">
+        
+        <div class="left_banner">
+            
+            <h1 class="bannerh1">
+                Your complete social media management solution
+            </h1>
+            <p class="bannerp">We have the most talented social media manager and web designer. <br> we will manage your social life and also design your business website</p>
+            <a href="" class="bannercontact">contact us</a>
+            <a href="" class="bannerlearn">learn more</a>
+        </div>
+            <div class="right_banner">
+            
+           <img class="bannerimg" src="img/slider-img-01.png" alt="">
+            
+        </div>
+        
+    </div>
+    <div class="about_section">
+        <div class="leftaboutsection">
+            <h2 class="leftaboutsectionh2">About us</h2>
+            <p class="leftaboutsectionp">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, labore ratione perspiciatis sit rem aut tempore doloremque neque. Asperiores, tempore beatae atque optio deleniti eum. Non quibusdam eaque labore, fugiat eligendi delectus illo nisi impedit optio sapiente, perferendis veniam mollitia sint eos modi voluptatibus odio nemo suscipit animi sit esse ex. Vero aliquid explicabo velit minus quam, incidunt tenetur aut, itaque iusto voluptatibus magnam, molestias saepe enim labore delectus temporibus officia placeat sequi quas hic earum ut sit ratione asperiores. Delectus rem error dolorem cum qui repudiandae optio provident eaque ducimus dolores, at aliquid nemo, rerum suscipit. Suscipit asperiores, minus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas molestiae laborum quasi dolores, aliquid quidem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam esse dolores soluta voluptate necessitatibus in nostrum, doloremque incidunt perspiciatis cum libero nam hic numquam alias.
+            </p>
+        </div>
+        <div class="rightaboutsection">
+            
+        </div>
+        
+    </div>
+    
+    <div class="service" id="serviceid">
+<!--        <h1 class="serviceh1">Our services</h1>-->
+        <div class="service1">
+           
+                <img class="service1img" src="icon/webdesign.png" alt="">
+                <h1 class="service1h1">Web design</h1>
+                <p class="service1p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At incidunt, molestiae deleniti enim voluptatibus, facere commodi rerum illo rem mollitia alias, error aliquam deserunt unde nemo beatae? Qui repellat, aliquid aspernatur, quaerat beatae cum delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet iste aperiam, assumenda in soluta, numquam.</p>
+                <a href="#" class="service1a1">Learn more</a>
+            
+            
+            
+        </div>
+        <div class="service2">
+            <img class="service2img" src="icon/management.png" alt="">
+                <h1 class="service2h1">Social megia management</h1>
+                <p class="service2p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At incidunt, molestiae deleniti enim voluptatibus, facere commodi rerum illo rem mollitia alias, error aliquam deserunt unde nemo beatae? Qui repellat, aliquid aspernatur, quaerat beatae cum delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet iste aperiam, assumenda in soluta, numquam.</p>
+                <a href="#" class="service2a1">Learn more</a>
+            
+        </div>
+        <div class="service3">
+            <img class="service3img" src="icon/marketing.png" alt="">
+                <h1 class="service3h1">Digital Marketing</h1>
+                <p class="service3p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At incidunt, molestiae deleniti enim voluptatibus, facere commodi rerum illo rem mollitia alias, error aliquam deserunt unde nemo beatae? Qui repellat, aliquid aspernatur, quaerat beatae cum delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet iste aperiam, assumenda in soluta, numquam.</p>
+                <a href="#" class="service3a1">Learn more</a>
+            
+        </div>
+    </div>
+    
+    
+    <div class="memberandcontact" id="teamid">
+        <div class="left_memberandcontact">
+            
+            <div class="team">
+                <h1 class="teamh1">Our team member</h1>
+                <div class="member">
+                   <div class="member_img_section">
+                    <img class="member_img1" src="img/mohot.jpg" alt="">
+                    </div>
+                    <div class="member_info">
+                        <h1 class="name">Mahfuj Mahtab Mohot</h1>
+                        <p class="position">website designer and digital marketer</p>
+                    </div>
+                    <div class="social">
+                        <ul>
+                            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                 <div class="member">
+                   <div class="member_img_section">
+                    <img class="member_img1" src="img/mohot.jpg" alt="">
+                    </div>
+                    <div class="member_info">
+                        <h1 class="name">Mahfuj Mahtab Mohot</h1>
+                        <p class="position">website designer and digital marketer</p>
+                    </div>
+                    <div class="social">
+                        <ul>
+                            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                
+                
+            </div>
+            <a href="" class="allmember">All member</a>
+            
+        </div>
+        <div class="right_memberandcontact" id="contactid">
+            <div class="contact">
+               <form action="" method="post">
+                <h2 class="contacth2">Please submit this form we will contact you soon</h2>
+                <div class="row">
+                    <div class="rowname">Name</div>
+                    <div class="rowinput"><input type="text" name="name" required></div>
+                </div>
+                
+                <div class="row">
+                    <div class="rowname">Email</div>
+                    <div class="rowinput">
+                        <input type="email" name="email" required>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="rowname">Number</div>
+                    <div class="rowinput">
+                        <input type="number" name="number" required>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="rowname">Website</div>
+                    <div class="rowinput">
+                        <input type="text" name="website">
+                    </div>
+                </div>
+                
+                <div class="msg">
+                    <div class="rowname">Message</div>
+                    <div class="rowinput">
+                        <textarea name="" id="" name="message" required></textarea>
+                    </div>
+                </div>
+                <div class="contact_button">
+                    <input class="contact_submit" type="submit" value="Submit A Message" name="msg_submit">
+                </div>
+               </form>
+        
+                
+            </div>
+        </div>
+    </div>
+    
+    <?php
+    
+    if(isset($_POST['msg_submit'])){
+    
+    $name=strip_tags($_POST['name']);
+    $email=$_POST['email'];
+    $number=$_POST['number'];
+    $website=strip_tags($_POST['website']);
+    $message=strip_tags($_POST['message']);
+     
+        if(!preg_match("/^[a-zA-Z ]*$/",$name)){
+            echo'<script>alert("only valid name please")</script>';
+        }
+                else{
+                 
+                    if(!preg_match("/^[a-zA-Z ]*$/",$website)){
+                        echo'<script>alert("only valid website please")</script>';
+                    }
+                    else {
+                         if(!preg_match("/^[a-zA-Z ]*$/",$message)){
+                        echo'<script>alert("only valid message please")</script>';
+                    }
+                    }
+                    
+                }
+            }
+            
+        
+    
+     
+        
+        
+        
+    
+    ?>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <div class="newsletter" id="newsletterid">
+        <img src="img/email.png" alt="" class="newsletterimg">
+        <h1 class="newsletterh1">Sign up for a Newsletter</h1>
+        <input type="email" class="newsletterinput" placeholder="Subscribe to our news letter">
+        <input type="submit" class="newslettersubmit" value="Subscribe">
+        
+        
+    </div>
+    
+    
+    <div class="footer">
+        <div class="footer1">
+            <h1 class="footer1h1">About us</h1>
+            <p class="footer1p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam atque nostrum adipisci voluptatibus, iure laborum nihil natus, et quam sapiente, perferendis praesentium optio reprehenderit quo ipsa. Ipsum magni sint quis laudantium eum! Ipsum aperiam, corrupti.</p>
+            
+            <div class="footer1social">
+                <ul>
+                    <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                </ul>
+            </div>
+            
+        </div>
+        <div class="footer2">
+            <h1 class="footer2h1">Our Contact information</h1>
+            <h2 class="footer2h21">Email:Team@metrosoftonic.com</h2>
+            <h2 class="footer2h22">phone:019t65677871</h2>
+            <h2 class="footer2h23">address:01987765571</h2>
+            
+        </div>
+    </div>
+    
+    <script>
+        
+    $(document).ready(function(){
+        
+        
+        $('.bar').click(function(){
+           $('.header2').toggle('slow');
+        })
+        
+    })
+    
+    
+    
+    
+    
+    </script>    
+</body>
+</html>
---
+++ social_manage.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home-Metrosoftonic.com</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/5d4ee011b6.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Arimo&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Heebo&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Heebo&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/responsive.css">
+</head>
+<body>
+    <div class="bar"><i class="fas fa-bars"></i></div>
+    <div class="header">
+        <div class="header1">
+            <div class="left_header1"><a href="">Logo</a></div>
+            <div class="right_header1">
+                <div id="header_section1">
+                    <ul>
+                        <li><span class="span1">Email Us</span> <a href="mailto:team@metrosoftonic.com"><i class="fas fa-envelope"></i>team@metrosoftonic.com</a></li>
+                       
+                    </ul>
+                    
+                </div>
+                <div id="header_section2">
+                     
+                    <ul>
+                        <li><span>Call Us</span> <a href="callto:01966213571"><i class="fas fa-phone-alt"></i>01966213571</a></li>
+                       
+                    </ul>
+                    
+                    
+                </div>
+                <div id="header_section3">
+                    
+                    <ul>
+                        <li><span>Follow us</span>
+                        
+                        <ul>
+                            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                            
+                        </ul>
+                        
+                        
+                        </li>
+                    </ul>
+                    
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div class="header2">
+           <div class="left_header2">
+            <ul>
+                <li><a href=""><i class="fas fa-home"></i>Home</a></li>
+                <li><a href="#aboutid"><i class="fas fa-address-card"></i>about</a></li>
+                <li><a href="#teamid"><i class="fas fa-users"></i>team</a></li>
+                <li><a href="#contactid">contact<i class="fas fa-arrow-down"></i></a></li>
+                <li><a  href="#serviceid">service<i class="fas fa-arrow-down"></i></a></li>
+                
+            </ul>
+            
+            </div>
+            
+            <div class="right_header2">
+                <ul>
+                    <li><a href="#newsletterid">get a free quote</a></li>
+                    <li><div class="arrow"></div></li>
+                </ul>
+            </div>
+            
+        </div>
+        
+    </div>
+    
+   <div class="banner">
+        
+        <div class="left_banner">
+            
+            <h1 class="bannerh1">
+                Your complete social media management solution
+            </h1>
+            <p class="bannerp">We have the most talented social media manager and web designer. <br> we will manage your social life and also design your business website</p>
+            <a href="" class="bannercontact">contact us</a>
+            <a href="" class="bannerlearn">learn more</a>
+        </div>
+            <div class="right_banner">
+            
+           <img class="bannerimg" src="img/slider-img-01.png" alt="">
+            
+        </div>
+        
+    </div>
+  
+    <div class="footer">
+        <div class="footer1">
+            <h1 class="footer1h1">About us</h1>
+            <p class="footer1p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam atque nostrum adipisci voluptatibus, iure laborum nihil natus, et quam sapiente, perferendis praesentium optio reprehenderit quo ipsa. Ipsum magni sint quis laudantium eum! Ipsum aperiam, corrupti.</p>
+            
+            <div class="footer1social">
+                <ul>
+                    <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                </ul>
+            </div>
+            
+        </div>
+        <div class="footer2">
+            <h1 class="footer2h1">Our Contact information</h1>
+            <h2 class="footer2h21">Email:Team@metrosoftonic.com</h2>
+            <h2 class="footer2h22">phone:01966213571</h2>
+            <h2 class="footer2h23">address:01966213571</h2>
+            
+        </div>
+    </div>
+    
+    <script>
+        
+    $(document).ready(function(){
+        
+        
+        $('.bar').click(function(){
+           $('.header2').toggle('slow');
+        })
+        
+    })
+    
+    
+    
+    
+    
+    </script>    
+</body>
+</html>
